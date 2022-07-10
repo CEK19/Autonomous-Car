1889383912a9fb4e9a396b3dbf20f406f4270ada
MinhTuLeHoang <minhtulehoang1302@gmail.com>
2022-07-10 07:49:33 +0700

    wrap perpestive

--- kot3_pkg/dectectByParabok/main.py
+++ kot3_pkg/dectectByParabok/main.py
@@ -2,8 +2,10 @@ import cv2 as cv
 from matplotlib.ft2font import HORIZONTAL
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 import sys
 import numpy
+
 
 # def lanesDetection(img):
 #     # img = cv.imread("./img/road.jpg")
@@ -56,20 +58,20 @@ def laneDetection(img):
     HORIZONTAL = img.shape[1]
     
     grayImage = cv.cvtColor(img, cv.COLOR_RGB2GRAY)    
-    cv.imshow("Gray" , grayImage)        
+    # cv.imshow("Gray" , grayImage)
     
     # https://stackoverflow.com/questions/21324950/how-can-i-select-the-best-set-of-parameters-in-the-canny-edge-detection-algorith
     highThresh, thresh_im = cv.threshold(grayImage, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     lowThresh = 0.5*highThresh    
     edgeDetectionImage = cv.Canny(grayImage, lowThresh, highThresh, 
                                   apertureSize=3)        
-    cv.imshow("egde", edgeDetectionImage)
+    # cv.imshow("egde", edgeDetectionImage)
     lines = cv.HoughLinesP(edgeDetectionImage, 
                            rho=2, theta=np.pi/180,
                            threshold=30, lines=np.array([]), 
                            minLineLength=10, maxLineGap=50)
     imageWithLine = draw_lines(img, lines)    
-    cv.imshow("Line hough transfom", imageWithLine)
+    # cv.imshow("Line hough transfom", imageWithLine)
     
     
     statisticMatrix = np.sum(edgeDetectionImage, axis=0) # CACULATE ALL COLUMN IN MATRIX
@@ -90,13 +92,79 @@ def laneDetection(img):
     print((leftStartPointX, leftStartPointY))
     print((rightStartPointX, rightStartPointY))
     
-    imageCircle = cv.circle(img, (leftStartPointX, leftStartPointY), 
-                            20, (0, 255, 0), 10)
-    imageCircle = cv.circle(imageCircle, (rightStartPointX, rightStartPointY), 
-                            20, (0, 255, 0), 10)
-    imageCircle = cv.circle(imageCircle, (0, 0), 
-                        20, (0, 255, 0), 10)
-    cv.imshow("Circle for fun", imageCircle) 
+    # imageCircle = cv.circle(img, (leftStartPointX, leftStartPointY), 
+    #                         20, (0, 255, 0), 10)
+    # imageCircle = cv.circle(imageCircle, (rightStartPointX, rightStartPointY), 
+    #                         20, (0, 255, 0), 10)
+    # imageCircle = cv.circle(imageCircle, (0, 0), 
+    #                     20, (0, 255, 0), 10)
+
+    # # point
+
+    # # right bottom
+    # imageCircle = cv.circle(imageCircle, (840, 368), 
+    #                     20, (0, 0, 255), 10)
+
+    # # left bottom
+    # imageCircle = cv.circle(imageCircle, (450, 368), 
+    #                     20, (0, 0, 255), 10)
+    
+    # # right top
+    # imageCircle = cv.circle(imageCircle, (668, 226), 
+    #                     20, (0, 0, 255), 10)
+    
+
+    # # left top
+    # imageCircle = cv.circle(imageCircle, (578, 226), 
+    #                     20, (0, 0, 255), 10)
+
+    # cv.imshow("check", imageCircle)
+    
+    # imgplot = plt.imshow(imageCircle)
+    # plt.show()
+
+    left_bot = {"x":450, "y":368} #[x, y]
+    left_top = {"x":578, "y":226}
+    right_top = {"x":668, "y":226}
+    right_bot = {"x":840, "y":368}
+
+    # print(left_bot["x"])
+    
+    srcPoint = np.float32([
+        [left_bot["x"], left_bot["y"]], 
+        [left_top["x"], left_top["y"]],
+        [right_top["x"], right_top["y"]],
+        [right_bot["x"], right_bot["y"]]
+    ])
+    # print(srcPoint)
+    # dstPoint = np.float32([
+    #     [left_bot["x"]//2 + left_top["x"]//2, left_bot["y"]],
+    #     [left_bot["x"]//2 + left_top["x"]//2, left_top["y"]],
+    #     [right_bot["x"]//2 + right_top["x"]//2, right_top["y"]],
+    #     [right_bot["x"]//2 + right_top["x"]//2, right_bot["y"]],
+    # ])
+
+    dstPoint = np.float32([
+        [left_bot["x"], left_bot["y"]],
+        [left_bot["x"], left_top["y"]],
+        [right_bot["x"], right_top["y"]],
+        [right_bot["x"], right_bot["y"]],
+    ])
+
+    matrix = cv.getPerspectiveTransform(srcPoint, dstPoint)
+    result = cv.warpPerspective(img, matrix, (HORIZONTAL, VERTICAL))
+    cv.imshow("perpecstive", result)
+
+    # imgplot = plt.imshow(result)
+    # plt.show()
+
+    crop_img = result[0:361, 400:850]
+    cv.imshow("crop", crop_img)
+
+
+    
+
+    # cv.imshow("Circle for fun", imageCircle) 
     cv.waitKey(0)
     cv.destroyAllWindows() 
     
@@ -114,7 +182,8 @@ def videoLanes():
     cv.destroyAllWindows()
 
 def imageLanes():
-    img = cv.imread("./img/demo/page3.png")
+    # img = cv.imread("./img/demo/page3.png")
+    img = cv.imread("/home/minhtulehoang/kitty/testing/image_2/um_000000.png")
     processedImg = laneDetection(img=img)
 
 
