32926acf415d8c2c0fb68f2f1332bc6acd0b8937
CEK19 <nguyentrongnhan2001.work@gmail.com>
2022-11-02 07:55:56 +0700

    #11 1/11/2022: template rl

--- src/objectMovementDetection/table.py
+++ src/objectMovementDetection/table.py
@@ -1,25 +1,43 @@
 from const import *
+
+
 class RLAlgorithm:
-    def __init__(self, rayCastingData, actions) -> None:                
-        # Converting n raycasting to signal in area , min raycast of each area        
+    def __init__(self, rayCastingData, actions) -> None:
+        # Converting n raycasting to signal in area , min raycast of each area
+        self.signalPerAreaData = self._initSignalPerArea(rayCastingData=rayCastingData)
+                
+        self.Q = self._initQTable(actions=actions)
+        
+    def _initSignalPerArea(self, rayCastingData):
         div = len(rayCastingData) // RLParam.AREA_RAY_CASTING_NUMBERS
         mod = len(rayCastingData) % RLParam.AREA_RAY_CASTING_NUMBERS
-        self.signalPerAreaData = [0] * (RLParam.AREA_RAY_CASTING_NUMBERS + (0 if mod == 0  else  1))        
-        
+
+        tmpData = [0] * (RLParam.AREA_RAY_CASTING_NUMBERS +
+                            (0 if mod == 0 else 1))
+
         tmpCount = 0
-        for i in range(len(self.signalPerAreaData)):
+        for i in range(len(tmpData)):
             tmp = []
             for _ in range(div):
                 tmp.append(rayCastingData[tmpCount])
                 tmpCount += 1
-                if (tmpCount == len(rayCastingData)): 
+                if (tmpCount == len(rayCastingData)):
                     break
-            self.signalPerAreaData[i] = min(tmp)
-        
-        self.Q = {
+            tmpData[i] = min(tmp)
+        return tmpData        
 
-        }
+    def _initQTable(self, actions):
+        pass
+    
+    def _hashFromDistanceToState(self): # Tu
+        pass
+    
+    def getReward(currState, currAction): 
+        pass
+
+    def train(self):
+        pass
         
         
-RL = RLAlgorithm(range(91), [])
+RL = RLAlgorithm(range(90), [])
 print(RL.signalPerAreaData)
