d127b8c8dd0e3021c18a7414a0e5db745970231a
CEK19 <nguyentrongnhan2001.work@gmail.com>
2023-02-05 13:45:45 +0700

    last code of

--- src/lane-detect/ml_core2.py
+++ src/lane-detect/ml_core2.py
@@ -8,10 +8,19 @@ import random
 
 orgImgPath = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/image/"
 labelImgPath = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/label/"
-preprocessingPath = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/preprocessing/"
-visualizeResultPath = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/finalResult/"
-statisticLeftRoad = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/statisticLeftRoad/"
-statisticRightRoad = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/statisticRightRoad/"
+
+# preprocessingPath = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/preprocessing/"
+# visualizeResultPath = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/finalResult/"
+# timeCompare = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/timeCompare/"
+grayScale_1 = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/1-grayscale/"
+gaussCanny_2 = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/2-gauss-canny/"
+threshHold_3 =  "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/3-threshhold/"
+countour_4 = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/4-countour/"
+maxCountour_5 =  "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/5-maxcontour/"
+cropImg_5_5 = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/5.5-crop-img/"
+masking_6 = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/6-masking/"
+regression_7 = "/Users/mac/Desktop/EXTERNAL/NCKH/Autonomous-Car/src/lane-detect/fullOutput/7-regression/"
+currentLabel = ""
 
 
 def getIntersectionOfLines(coef1s, coef2s):
@@ -43,6 +52,7 @@ def removingOutliners(data1D, m = 5.0):
     return np.where(s < m)
 
 def algorithm(preprocessImage, centerPoint):
+    writeImage(pathFolder=cropImg_5_5, image=preprocessImage, prefix=currentLabel)
     col = preprocessImage.shape[1]
     row = preprocessImage.shape[0]
     centerX = col//2
@@ -66,6 +76,7 @@ def algorithm(preprocessImage, centerPoint):
 
     pts = pts.reshape((-1, 1, 2))
     cv2.fillPoly(cdstP, pts=[pts], color=0)
+    writeImage(pathFolder=masking_6, image=cdstP, prefix=currentLabel)
 
     positionYs, positionXs = np.where(cdstP == 255)
 
@@ -78,13 +89,6 @@ def algorithm(preprocessImage, centerPoint):
     # find best fit line right points in images
     arrayRightX, arrayRightY = positionXs[rightCondition], positionYs[rightCondition]
     
-    # plt.scatter(arrayLeftX, arrayLeftY, c="blue")
-    # plt.savefig(statisticLeftRoad + "statLeft" +str(time.time()) + ".jpg", bbox_inches='tight')
-    # plt.clf()
-    
-    # plt.scatter(arrayRightX, arrayRightY, c="red")
-    # plt.savefig(statisticRightRoad + "statRight" +str(time.time()) + ".jpg", bbox_inches='tight')
-    # plt.clf()    
 
     if (len(arrayLeftX) <= 10 or len(arrayRightX) <=10):
         return -1, None, None, None, None
@@ -120,24 +124,32 @@ def algorithm(preprocessImage, centerPoint):
     if (fromRightX > col):
         xMax = fromRightX
 
-    # plt.xlim([xMin, xMax])
-    # plt.plot([fromLeftX, targetLeftX], [fromLeftY, targetLeftY], color='red', linewidth=3)
-    # plt.plot([fromRightX, targetRightX],  [fromRightY, targetRightY], color='red', linewidth=3)
-    # plt.imshow(cdstP)
-    # plt.savefig(visualizeResultPath + "final" +str(time.time()) + ".jpg", bbox_inches='tight')
-    # plt.clf()
+    plt.xlim([xMin, xMax])
+    plt.plot([fromLeftX, targetLeftX], [fromLeftY, targetLeftY], color='red', linewidth=3)
+    plt.plot([fromRightX, targetRightX],  [fromRightY, targetRightY], color='red', linewidth=3)
+    plt.imshow(cdstP)
+    plt.savefig(regression_7 + "final" + currentLabel  +str(time.time()) + ".jpg", bbox_inches='tight')
+    plt.clf()
 
     return centerPoint[0]/abs(xMax - xMin), aL, bL, aR, bR
 
 
+def writeImage(pathFolder, image, prefix=""):
+    cv2.imwrite(pathFolder + prefix + str(time.time()) +".jpg", image)    
+    
+
 def preprocessing(frame):
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    writeImage(pathFolder=grayScale_1, image=gray, prefix=currentLabel)
 
     gaussianBlur = cv2.GaussianBlur(gray, (5, 5), 0)
     cannyEdgeDetection = cv2.Canny(gaussianBlur, 100, 200)
+    writeImage(pathFolder=gaussCanny_2, image=cannyEdgeDetection, prefix=currentLabel)
 
     _, thresh = cv2.threshold(gray, 100, 180, cv2.THRESH_BINARY)
+    writeImage(pathFolder=threshHold_3, image=thresh, prefix=currentLabel)
+    
     contours, _ = cv2.findContours(
         image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
     frameCopy = cannyEdgeDetection.copy()
@@ -165,6 +177,7 @@ def preprocessing(frame):
     if maxBoundAreaRect == -1:
         # cv2.imwrite(preprocessingPath + "err-" + str(time.time()) +
         #             ".jpg", cv2.vconcat([frameCopy, cannyEdgeDetection]))
+        writeImage(pathFolder=maxCountour_5, image=frameCopy, prefix="err" + currentLabel)
         return noMeaningValue, None, None, None
     else:
         x, y, w, h = int(maxBoundRect[0]), int(maxBoundRect[1]), int(
@@ -173,38 +186,48 @@ def preprocessing(frame):
         centerPoint = (frameCopy.shape[0]//2, 0)
         if w < 0.6*frame.shape[0] or \
                 y + h < 0.85*frame.shape[1]:
-            # cv2.rectangle(frameCopy, (x, y), (x + w, y + h), 0, 2)
-            # cv2.imwrite(preprocessingPath + "err-" + str(time.time()
-            #                                              ) + ".jpg", cv2.vconcat([frameCopy, cannyEdgeDetection]))
+            cv2.rectangle(frameCopy, (x, y), (x + w, y + h), 255, 2)
+            writeImage(pathFolder=maxCountour_5, image=frameCopy, prefix="err" + currentLabel)
             return noMeaningValue, None, None, None
 
-        # cv2.rectangle(frameCopy, (x, y), (x + w, y + h), 255, 2)
-        # cv2.imwrite(preprocessingPath + str(time.time()) +
-        #             ".jpg", cv2.vconcat([frameCopy, cannyEdgeDetection]))
-        return frameCopy[y: y + h, x: x + w], centerPoint, x, y
+        prevFrame = frameCopy.copy()
+        cv2.rectangle(frameCopy, (x, y), (x + w, y + h), 255, 2)
+        writeImage(pathFolder=maxCountour_5, image=frameCopy, prefix=currentLabel)
+                
+        return prevFrame[y: y + h, x: x + w], centerPoint, x, y
 
 
 # listOrgImages = load_images_from_folder(pathFolder=orgImgPath)
 listLabelImages = sorted(load_images_from_folder(pathFolder=labelImgPath))
-file = open("nhan-backend.txt", 'a')
-
-for singleLabelImage in listLabelImages:
+# file = open("nhan-backend.txt", 'a')
+# file = open(timeCompare + "nhan-time.txt", 'a')
+# valid = [53, 59, 60, 61, 65, 74, 81, 94, 172, 179, 215]
+for idx, singleLabelImage in enumerate(listLabelImages):
+    # if (idx not in valid):
+    #     continue
+    currentLabel = singleLabelImage
     fullPath = labelImgPath + singleLabelImage
     frame = cv2.imread(fullPath)
+    
+    startTime = time.time()    
     detectedRoad, centerPoint, x, y = preprocessing(frame=frame)
+    
     ratio = -1    
     aL, bL, aR, bR = 0, 0 , 0 , 0
     if (detectedRoad.any() == None):
         pass
     else:
         ratio, aL, bL, aR, bR = algorithm(detectedRoad, centerPoint=centerPoint)        
+        
+    endTime = time.time()        
+    # file.write(f'{endTime - startTime}\n')
 
-    if (ratio == -1):
-        file.write(f'{singleLabelImage}: left: invalid, right: invalid, x: invalud, y: invalid\n')    
-    else:
-        file.write(f'{singleLabelImage}: left: y={aL}x+{bL}, right: y={aR}x+{bR}, x:{x}, y:{y}\n')    
+    # if (ratio == -1):
+    #     file.write(f'{singleLabelImage}: left: invalid, right: invalid, x: invalud, y: invalid\n')    
+    # else:
+    #     file.write(f'{singleLabelImage}: left: y={aL}x+{bL}, right: y={aR}x+{bR}, x:{x}, y:{y}\n')    
     # file.write(f'{singleLabelImage}: {ratio}\n')
     # cv2.imshow("single label image", frame)
     if cv2.waitKey(25) == 27:
         break
-file.close()
+# file.close()
