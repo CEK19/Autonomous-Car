df60adb72040ac9a7075dc9736c6d8c4b4427fc6
CEK19 <nguyentrongnhan2001.work@gmail.com>
2023-02-07 21:42:57 +0700

    refractor all of car, remaining lidar and reward and step function

--- src/objectMovementDetection/gym_game/envs/pygame_2d.py
+++ src/objectMovementDetection/gym_game/envs/pygame_2d.py
@@ -1,7 +1,6 @@
 import math
 import pygame
 from sys import exit
-import random
 from const import *
 from utils import *
 
@@ -12,7 +11,7 @@ class Lidar():
 
 
 class Car():
-    def __init__(self, initX, initY, maxForwardVelocity, maxRotationVelocity, accelerationForward, accelerationRotate) -> None:
+    def __init__(self, initX, initY, maxForwardVelocity, maxRotationVelocity, accelerationForward, accelerationRotate, radiusObject) -> None:
         self.xPos, self.yPos = initX, initY
         self.maxForwardVelocity = maxForwardVelocity
         self.maxRotationVelocity = maxRotationVelocity
@@ -23,6 +22,8 @@ class Car():
         self.currAngle = math.pi
         self.accelerationForward = accelerationForward
         self.accelerationRotate = accelerationRotate
+
+        self.radiusObject = radiusObject
 
     def move(self, action):
         if action == ACTIONS.FORWARD_ACCELERATION:
@@ -37,6 +38,9 @@ class Car():
         elif action == ACTIONS.TURN_RIGHT_ACCELERATION:
             self.currRotationVelocity = min(
                 self.currRotationVelocity + self.accelerationRotate, PLAYER_SETTING.MAX_ROTATION_VELO)
+        elif action == ACTIONS.STOP:
+            self.currentForwardVelocity = 0
+            self.currRotationVelocity = 0
         elif action == ACTIONS.DO_NOTHING:
             pass
 
@@ -52,20 +56,37 @@ class Car():
             self.currAngle = abs(self.currAngle - 2*math.pi)
 
         # Update the new position based on the velocity
-        self.yPos += math.cos(self.currAngle) * self.currVelocity * dt
-        self.xPos += -math.sin(self.currAngle) * self.currVelocity * dt
+        self.yPos += math.cos(self.currAngle) * \
+            self.currentForwardVelocity * dt
+        self.xPos += -math.sin(self.currAngle) * \
+            self.currentForwardVelocity * dt
 
     def checkCollision(self, collisions):
+        if collisions == None or len(collisions):
+            return False
+
         for collision in collisions:
             distanceBetweenCenter = Utils.distanceBetweenTwoPoints(
                 self.xPos, self.yPos, collision.xPos, collision.yPos)
             # https://stackoverflow.com/questions/22135712/pygame-collision-detection-with-two-circles
             if distanceBetweenCenter <= 2*PLAYER_SETTING.RADIUS_OBJECT:
                 return True
+
         return False
 
     def _rayCasting(self, collisions):
         pass
+
+    def draw(self, screen):
+        # draw player on 2D board
+        pygame.draw.circle(
+            screen, COLOR.RED, (self.xPos, self.yPos), self.radiusObject
+        )
+
+        # draw player direction
+        pygame.draw.line(screen, COLOR.GREEN, (self.xPos, self.yPos),
+                         (self.xPos - math.sin(self.currAngle)*20,
+                          self.yPos + math.cos(self.currAngle)*20), 3)
 
 
 class PyGame2D():
@@ -74,88 +95,78 @@ class PyGame2D():
         self.screen = pygame.display.set_mode(
             (GAME_SETTING.SCREEN_WIDTH, GAME_SETTING.SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.robot = Car()
+        self.robot = Car(
+            initX=PLAYER_SETTING.INITIAL_X,
+            initY=PLAYER_SETTING.INITIAL_Y,
+            maxForwardVelocity=PLAYER_SETTING.MAX_FORWARD_VELO,
+            maxRotationVelocity=PLAYER_SETTING.MAX_ROTATION_VELO,
+            accelerationForward=PLAYER_SETTING.ACCELERATION_FORWARD,
+            accelerationRotate=PLAYER_SETTING.ACCELERATION_ROTATE,
+            radiusObject=PLAYER_SETTING.RADIUS_OBJECT,
+        )
         self.obstacles = self._initObstacle()
         self.mode = 0
 
-    def _initObstacle():
+    def _initObstacle(self):
         pass
 
     def action(self, action):
         self.robot.move(action=action)
-
-        # TODO: UPDATE IT
-        self.car.update()
-        self.car.checkCollision(collisions=self.obstacles)
-
-        self.car.radars.clear()
-        for d in range(-90, 120, 45):
-            self.car.check_radar(d)
+        self.robot.checkCollision(collisions=self.obstacles)
 
     def evaluate(self):
         # TODO: UPDATE IT
         reward = 0
         """
-        if self.car.check_flag:
-            self.car.check_flag = False
-            reward = 2000 - self.car.time_spent
-            self.car.time_spent = 0
+        if self.robot.check_flag:
+            self.robot.check_flag = False
+            reward = 2000 - self.robot.time_spent
+            self.robot.time_spent = 0
         """
-        if not self.car.is_alive:
-            reward = -10000 + self.car.distance
+        # if not self.robot.is_alive:
+        #     reward = -10000 + self.robot.distance
 
-        elif self.car.goal:
-            reward = 10000
-        return reward
+        # elif self.robot.goal:
+        #     reward = 10000
+        # return reward
 
     def is_done(self):
         # TODO: UPDATE IT
-        if not self.car.is_alive or self.car.goal:
-            self.car.current_check = 0
-            self.car.distance = 0
-            return True
-        return False
+        # if not self.robot.is_alive or self.robot.goal:
+        #     self.robot.current_check = 0
+        #     self.robot.distance = 0
+        #     return True
+        # return False
+        pass
 
     def observe(self):
+        pass
         # TODO: UPDATE IT
-        radars = self.car.radars
-        ret = [0, 0, 0, 0, 0]
-        for i, r in enumerate(radars):
-            ret[i] = int(r[1] / 30)
+        # radars = self.robot.radars
+        # ret = [0, 0, 0, 0, 0]
+        # for i, r in enumerate(radars):
+        #     ret[i] = int(r[1] / 30)
 
-        return tuple(ret)
+        # return tuple(ret)
 
     def view(self):
         # TODO: UPDATE IT
         # draw game
+        self.screen.fill(COLOR.BLACK)
+        self.screen.blit(self.screen, (0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_m:
-                    self.mode += 1
-                    self.mode = self.mode % 3
+                pygame.quit()
+                exit()
 
-        self.screen.blit(self.car.map, (0, 0))
-
-        if self.mode == 1:
-            self.screen.fill((0, 0, 0))
-
-        self.car.radars_for_draw.clear()
-        for d in range(-90, 120, 45):
-            self.car.check_radar_for_draw(d)
-
-        pygame.draw.circle(self.screen, (255, 255, 0),
-                           check_point[self.car.current_check], 70, 1)
-        self.car.draw_collision(self.screen)
-        self.car.draw_radar(self.screen)
-        self.car.draw(self.screen)
-
-        text = self.font.render(
-            "Press 'm' to change view mode", True, (255, 255, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (screen_width/2, 100)
-        self.screen.blit(text, text_rect)
-
+        self.robot.draw(screen=self.screen)
         pygame.display.flip()
-        self.clock.tick(self.game_speed)
+        self.clock.tick(GAME_SETTING.FPS)
+
+
+game = PyGame2D()
+while True:
+    Utils.inputUser(game)
+    game.view()
+    pass
--- src/objectMovementDetection/gym_game/envs/utils.py
+++ src/objectMovementDetection/gym_game/envs/utils.py
@@ -2,6 +2,7 @@ import pygame
 import math
 from const import *
 
+
 class Utils:
     @staticmethod
     def blit_rotate_center(win, image, top_left, angle):
@@ -9,11 +10,11 @@ class Utils:
         new_rect = rotated_image.get_rect(
             center=image.get_rect(topleft=top_left).center)
         win.blit(rotated_image, new_rect.topleft)
-        
+
     @staticmethod
     def distanceBetweenTwoPoints(xPointA, yPointA, xPointB, yPointB):
         return math.sqrt((xPointA - xPointB)**2 + (yPointA - yPointB)**2)
-    
+
     @staticmethod
     def findLinePassTwoPoints(xPointA, yPointA, xPointB, yPointB):
         # y = ax + b
@@ -34,29 +35,30 @@ class Utils:
             return EQUATION.ONE_SOLUTION, -b/(2*a), -b/(2*a)
         else:
             return EQUATION.TWO_SOLUTION, (-b + math.sqrt(delta))/(2*a), (-b - math.sqrt(delta))/(2*a)
-    
+
     @staticmethod
     def getDistanceFromObstacle(xCenter, yCenter, xTarget, yTarget, xObstacle, yObstacle):
         # if abs(xTarget - xCenter) < 0.0001:
-            # x = xCenter
-            # (xCenter - xObstacle)^2 + (y - yObstacle)^2 = r^2
+        # x = xCenter
+        # (xCenter - xObstacle)^2 + (y - yObstacle)^2 = r^2
         # Pt đường thẳng lidar y = ax + b
         a, b = Utils.findLinePassTwoPoints(xCenter, yCenter, xTarget, yTarget)
         # print("42: ", Utils.verifyLine(a, b, xCenter, yCenter))
         # print("41: ", Utils.verifyLine(a, b, xTarget, yTarget))
         # print("y = {}x + {}".format(a, b))
-        
+
         # (x - xObstacle)^2 + (y - yObstacle)^2 = r^2
         # (x - xObstacle)^2 + (a*x + b - yObstacle)^2 = r^2
         # x^2 - 2*x*xObstacle + xObstacle^2 + a^2*x^2 + 2*a*x*(b - yObstacle) + (b - yObstacle)^2 = r^2
         # Pt đường thẳng cắt hình tròn (a^2 + 1)x^2 - 2*(xObstacle - a*b + a*yObstacle)x + (b - yObstacle)**2 + xObstacle**2 - RADIUS_LIDAR**2 = 0
         a_temp = a**2 + 1
         b_temp = -2*xObstacle + 2*a*(b - yObstacle)
-        c_temp = (b - yObstacle)**2 + xObstacle**2 - (PLAYER_SETTING.RADIUS_OBJECT)**2
+        c_temp = (b - yObstacle)**2 + xObstacle**2 - \
+            (PLAYER_SETTING.RADIUS_OBJECT)**2
         # print("a_temp = {}, b_temp = {}, c_temp = {}".format(a_temp, b_temp, c_temp))
-        numberOfSolution, x1, x2 = Utils.findSolOfEquation(a_temp, b_temp, c_temp)
-        
-        
+        numberOfSolution, x1, x2 = Utils.findSolOfEquation(
+            a_temp, b_temp, c_temp)
+
         if numberOfSolution == EQUATION.NO_SOLUTION:
             # print('NO_SOLUTION')
             return PLAYER_SETTING.INFINITY
@@ -80,7 +82,7 @@ class Utils:
             # print('---length---')
             # print(d1, d2)
             return d1 if d1 <= d2 else d2
-    
+
     @staticmethod
     def verifyLine(a, b, x, y):
         # y = ax + b
@@ -89,3 +91,27 @@ class Utils:
     @staticmethod
     def verifyCircle(xCenter, yCenter, radius, x, y):
         return (x - xCenter)**2 + (y - yCenter)**2 - radius**2
+
+    @staticmethod
+    def inputUser(game):
+        keys = pygame.key.get_pressed()
+        # Rotate left ()
+        if keys[pygame.K_a]:
+            game.action(ACTIONS.TURN_LEFT_ACCELERATION)
+        # Rotate right ()
+        elif keys[pygame.K_d]:
+            game.action(ACTIONS.TURN_RIGHT_ACCELERATION)
+        # Increase forward velocity
+        elif keys[pygame.K_w]:
+            game.action(ACTIONS.FORWARD_ACCELERATION)
+        elif keys[pygame.K_x]:
+            game.action(ACTIONS.BACKWARD_ACCELERATION)
+        # Stop
+        elif keys[pygame.K_s]:
+            game.action(ACTIONS.STOP)
+        else:
+            game.action(ACTIONS.DO_NOTHING)
+            
+    @staticmethod
+    def debugError(params):
+        print(params)
