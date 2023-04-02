d85883db0dd3c1d86979978f19e3a375acd3fe12
MinhTuLeHoang <minhtulehoang1302@gmail.com>
2023-04-01 20:54:04 -0700

    add init node

---
+++ kot3_pkg/scripts/init.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+NODE_NAME_INIT = rospy.get_param("NODE_NAME_INIT")
+
+def callback(data):
+    print("aaaa", data)
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def listener():
+
+    rospy.init_node(NODE_NAME_INIT = rospy.get_param("NODE_NAME_INIT")
+, anonymous=True)
+
+    # rospy.Subscriber('chatter', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
