7a51d50cf2fd9418c721d3cd8e79727d874be576
MinhTuLeHoang <minhtulehoang1302@gmail.com>
2023-04-07 10:04:03 -0700

    done rotation

--- kot3_pkg/scripts/ros_combine_lidar_lane.py
+++ kot3_pkg/scripts/ros_combine_lidar_lane.py
@@ -38,10 +38,11 @@ WIDTH_OPTIMAL_PATH = 50
 HEIGH_OPTIMAL_PATH = 50
 BLOCKED_COLOR = 255
 NON_BLOCKED_COLOR = 0
-DELTA = 14 # 50
+LANE_THICKNESS = 1
+DELTA = 35 # 50 / 14
 DELTA_X = DELTA
 DELTA_Y = DELTA
-NUM_POINTS_OF_DIRECTION = 12 # 35
+NUM_POINTS_OF_DIRECTION = 25 # 35 / 12
 MAX_STRAIGHT_VELOCITY = 0.05  # 0.2
 MAX_TURN_VELOCITY = 2.0  # 2.0
 
@@ -55,19 +56,51 @@ class Utils:
         return B[0] - A[0], B[1] - A[1]
     
     @staticmethod
+    def getInvertVector(A):
+        return -A[0], -A[1]
+    
+    @staticmethod
+    def getRotatedPoint(point, center, angle):
+        newX = center[0] + (point[0] - center[0]) * math.cos(angle) - (point[1] - center[1]) * math.sin(angle)
+        newY = center[1] + (point[0] - center[0]) * math.sin(angle) + (point[1] - center[1]) * math.cos(angle)
+        return int(newX), int(newY)
+    
+    @staticmethod
     def distanceBetweenPoints(A, B):
         return math.sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
     
     @staticmethod
     def getAngleOfVectors(A, B):
         return np.arccos(np.dot(A, B)/(np.linalg.norm(A)*np.linalg.norm(B)))
+    
+    # y = Ax + b || Ax - y + b
+    @staticmethod
+    def isPointBetween2Lane(leftLaneA, leftLaneB, rightLaneA, rightLaneB, point):
+        if point[1] > leftLaneA*point[0] + leftLaneB and point[1] > rightLaneA*point[0] + rightLaneB:
+            return True
+        return False
+    
+    # y = Ax + b || Ax - y + b
+    @staticmethod
+    def getEquationOfLane(point1, point2):
+        A = (point1[1] - point2[1]) / (point1[0] - point2[0] + 0.0001)
+        B = point1[1] - A*point1[0]
+        return A, B
+    
+    # 3.5m = 350cm = 100pixels
+    @staticmethod
+    def convertMetToPixel(m):
+        return m*100
+    
+    @staticmethod
+    def convertPixelToMet(pixel):
+        return pixel/100
 
     @staticmethod
     def publicVelocity(straight, angular):
         myTwist = Twist()
         myTwist.linear.x = straight
         myTwist.angular.z = angular
-        # print(myTwist)
         pub.publish(myTwist)
         # msg = json.dumps({"linear": straight, "angular": angular})
         # pub.publish(msg)
@@ -94,10 +127,11 @@ class CombineLidarLane:
         self.goal2Y = None
 
         # Position of lane
+        self.lastTimeReciveLane = time.time()
         self.leftBottomLaneX = 0
         self.leftBottomLaneY = HEIGH_OPTIMAL_PATH - 1
 
-        self.leftTopLaneX = WIDTH_OPTIMAL_PATH//4
+        self.leftTopLaneX = WIDTH_OPTIMAL_PATH//10
         self.leftTopLaneY = 0
 
         self.rightTopLaneX = WIDTH_OPTIMAL_PATH - self.leftTopLaneX
@@ -105,12 +139,27 @@ class CombineLidarLane:
 
         self.rightBottomLaneX = WIDTH_OPTIMAL_PATH - 1
         self.rightBottomLaneY = HEIGH_OPTIMAL_PATH - 1 
+        
+        #  Pt duong thang y = Ax + b     =>      Ax - y + b = 0
+        self.leftLaneA = (self.leftTopLaneY - self.leftBottomLaneY) / \
+            (self.leftTopLaneX - self.leftBottomLaneX)
+        self.leftLaneB = self.leftTopLaneY - self.leftLaneA*self.leftTopLaneX
+
+        self.rightLaneA = (self.rightTopLaneY - self.rightBottomLaneY) / \
+            (self.rightTopLaneX - self.rightBottomLaneX)
+        self.rightLaneB = self.rightTopLaneY - self.rightLaneA * self.rightTopLaneX
 
     def drawLanesOnMap(self, simMap):
         color = 255
-        thickness = 5
-        simMap = cv2.line(simMap, (self.leftBottomLaneX, self.leftBottomLaneY), (self.leftTopLaneX, self.leftTopLaneY), color, thickness)
-        simMap = cv2.line(simMap, (self.rightBottomLaneX, self.rightBottomLaneY), (self.rightTopLaneX, self.rightTopLaneY), color, thickness)
+        
+        # Lane left: y = Ax + B
+        A, B = Utils.getEquationOfLane([self.leftBottomLaneX, self.leftBottomLaneY], [self.leftTopLaneX, self.leftTopLaneY])
+        
+        # Lane right: y = Cx + D
+        C, D = Utils.getEquationOfLane([self.rightBottomLaneX, self.rightBottomLaneY], [self.rightTopLaneX, self.rightTopLaneY])
+        
+        simMap = cv2.line(simMap, (round(-B/(A+0.0001)), 0), (round((HEIGH_OPTIMAL_PATH-1-B)/(A+0.0001)), HEIGH_OPTIMAL_PATH-1), color, LANE_THICKNESS)
+        simMap = cv2.line(simMap, (round(-D/(C+0.0001)), 0), (round((HEIGH_OPTIMAL_PATH-1-D)/(C+0.0001)), HEIGH_OPTIMAL_PATH-1), color, LANE_THICKNESS)
         return simMap
 
     def updateLaneDetectionSignal(self, msg):
@@ -138,10 +187,86 @@ class CombineLidarLane:
         # self.updateVelocity()
     
     def updateVelocity(self, event):
-        # self.getPath()
-        Utils.publicVelocity(self.straightVel, self.turnVel)
+        myTwist = Twist()
+        myTwist.linear.x = self.straightVel
+        myTwist.angular.z = self.turnVel
+        pub.publish(myTwist)
+        # msg = json.dumps({"linear": straight, "angular": angular})
+        # pub.publish(msg)
+        
+        # Utils.publicVelocity(self.straightVel, self.turnVel)
+        
+    def rotatePoint(self):
+        oldGoal = [self.goalX, self.goalY]
+        deltaTime = time.time() - self.lastTimeReciveLane
+        self.lastTimeReciveLane = time.time()
+        if self.turnVel == 0:
+            self.leftBottomLaneY = self.leftBottomLaneY + Utils.convertMetToPixel(self.straightVel) * deltaTime
+            self.rightBottomLaneY = self.rightBottomLaneY + Utils.convertMetToPixel(self.straightVel) * deltaTime
+            self.leftTopLaneY = self.leftTopLaneY + Utils.convertMetToPixel(self.straightVel) * deltaTime
+            self.rightTopLaneY = self.rightTopLaneY + Utils.convertMetToPixel(self.straightVel) * deltaTime
+            return
+        print("===== start rotate =====")
+        # R = self.straightVel / self.turnVel
+        R = Utils.convertMetToPixel(self.straightVel) / self.turnVel
+        isRight = self.turnVel > 0
+        print("isRight: ", isRight, ", angular: ", self.turnVel, ", R: ", R, ", deltaTime: ", deltaTime)
+        angular = self.turnVel * deltaTime
+        alpha = abs(angular)
+        curPos = [HEIGH_OPTIMAL_PATH // 2, WIDTH_OPTIMAL_PATH // 2]
+        newPos = [0, 0]
+        if isRight:
+            newPos = [curPos[0] + 2*R*math.sin(alpha/2)*math.cos(math.pi/2 - alpha/2), curPos[1] - 2*R*math.sin(alpha/2)*math.sin(math.pi/2 - alpha/2)]
+            # newPos = [curPos[0] + R + R*math.cos(alpha), curPos[1] + R*math.sin(alpha)]
+        else:
+            newPos = [curPos[0] - 2*R*math.sin(alpha/2)*math.cos(math.pi/2 - alpha/2), curPos[1] - 2*R*math.sin(alpha/2)*math.sin(math.pi/2 - alpha/2)]
+            # newPos = [curPos[0] - R - R*math.cos(alpha), curPos[1] + R*math.sin(alpha)]
+        vector = Utils.getVectorAB(curPos, newPos)
+        invertVec = Utils.getInvertVector(vector)
+        
+        leftBottom = [self.leftBottomLaneX + invertVec[0], self.leftBottomLaneY + invertVec[1]]
+        rightBottom = [self.rightBottomLaneX + invertVec[0], self.rightBottomLaneY + invertVec[1]]
+        leftTop = [self.leftTopLaneX + invertVec[0], self.leftTopLaneY + invertVec[1]]
+        rightTop = [self.rightTopLaneX + invertVec[0], self.rightTopLaneY + invertVec[1]]
+        # goal = [self.goalX + invertVec[0], self.goalY + invertVec[1]]
+        
+        # print old value
+        print("leftBottom old", self.leftBottomLaneX, self.leftBottomLaneY)
+        print("rightBottom old", self.rightBottomLaneX, self.rightBottomLaneY)
+        print("leftTop old", self.leftTopLaneX, self.leftTopLaneY)
+        print("rightTop old", self.rightTopLaneX, self.rightTopLaneY)
+        
+        print()
+        
+        self.leftBottomLaneX, self.leftBottomLaneY = Utils.getRotatedPoint(leftBottom, curPos, angular)
+        self.rightBottomLaneX, self.rightBottomLaneY = Utils.getRotatedPoint(rightBottom, curPos, angular)
+        self.leftTopLaneX, self.leftTopLaneY = Utils.getRotatedPoint(leftTop, curPos, angular)
+        self.rightTopLaneX, self.rightTopLaneY = Utils.getRotatedPoint(rightTop, curPos, angular)
+        # self.goalX, self.goalY = Utils.getRotatedPoint(goal, curPos, -angular)
+        # self.goalY = 0
+        # print("old goal: ", oldGoal)
+        # print("new goal: ", [self.goalX, self.goalY])
+        
+        print("leftBottom new", self.leftBottomLaneX, self.leftBottomLaneY)
+        print("rightBottom new", self.rightBottomLaneX, self.rightBottomLaneY)
+        print("leftTop new", self.leftTopLaneX, self.leftTopLaneY)
+        print("rightTop new", self.rightTopLaneX, self.rightTopLaneY)
+        
+        print("")
+        
+        print("leftBottom d", Utils.distanceBetweenPoints(curPos, leftBottom), Utils.distanceBetweenPoints(curPos, [self.leftBottomLaneX, self.leftBottomLaneY]))
+        print("rightBottom d", Utils.distanceBetweenPoints(curPos, rightBottom), Utils.distanceBetweenPoints(curPos, [self.rightBottomLaneX, self.rightBottomLaneY]))
+        print("leftTop d", Utils.distanceBetweenPoints(curPos, leftTop), Utils.distanceBetweenPoints(curPos, [self.leftTopLaneX, self.leftTopLaneY]))
+        print("rightTop d", Utils.distanceBetweenPoints(curPos, rightTop), Utils.distanceBetweenPoints(curPos, [self.rightTopLaneX, self.rightTopLaneY]))
+        
+        print()
+        
+        pass
                 
     def pathFinding(self, event):
+        print("start at", time.time())
+        self.rotatePoint()
+        
         # Convert lidar distance signal, result represent left to right (0 -> 179)
         angleList = np.arange(start=0, stop=360, step=1, dtype=np.int16)
         angleList = angleList * np.pi/180.
@@ -181,12 +306,14 @@ class CombineLidarLane:
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (DELTA, DELTA))
         simulateMap = cv2.dilate(simulateMap, kernel)
 
+        cv2.imshow("big Map", simulateMap)
+        
         # Cut the image in range 50px radius from robot (25px from left to 25px from right)
         simulateMap = simulateMap[WIDTH_SIMULATE_MAP//2 - WIDTH_OPTIMAL_PATH//2 : WIDTH_SIMULATE_MAP//2 + WIDTH_OPTIMAL_PATH//2, HEIGH_SIMULATE_MAP//2 - HEIGH_OPTIMAL_PATH//2 : HEIGH_SIMULATE_MAP//2 + HEIGH_OPTIMAL_PATH//2]
         pathOnlyMap = pathOnlyMap[WIDTH_SIMULATE_MAP//2 - WIDTH_OPTIMAL_PATH//2 : WIDTH_SIMULATE_MAP//2 + WIDTH_OPTIMAL_PATH//2, HEIGH_SIMULATE_MAP//2 - HEIGH_OPTIMAL_PATH//2 : HEIGH_SIMULATE_MAP//2 + HEIGH_OPTIMAL_PATH//2]
         
         # Draw path from lane detection in to map
-        # simulateMap = self.drawLanesOnMap(simulateMap)
+        simulateMap = self.drawLanesOnMap(simulateMap)
 
         # Path finding here
         # initalX, initialY = WIDTH_SIMULATE_MAP//2, HEIGH_SIMULATE_MAP - 1
@@ -279,6 +406,7 @@ class CombineLidarLane:
         # cv2.imshow("path only map", pathOnlyMap)
         cv2.imshow("path only map", cv2.resize(pathOnlyMap, (WIDTH_OPTIMAL_PATH*4, HEIGH_OPTIMAL_PATH*4)))
         self.getVelocity()
+        print("end at", time.time())
         if cv2.waitKey(1) == ord('q'):
             return   
 
@@ -299,8 +427,11 @@ class CombineLidarLane:
     def getVelocity(self):
         # point15th = len(self.tracePath) - 1 - NUM_POINTS_OF_DIRECTION
         point15th = NUM_POINTS_OF_DIRECTION
+        print(self.tracePath)
         if (point15th < 0 or not len(self.tracePath)):
             return
+        if point15th >= len(self.tracePath):
+            point15th = len(self.tracePath) - 1
         vecDirection = Utils.getVectorAB([HEIGH_OPTIMAL_PATH//2, int(WIDTH_OPTIMAL_PATH // 2)], self.tracePath[point15th])
         vecZero = (1, 0)
         angle = Utils.getAngleOfVectors(vecDirection, vecZero)
@@ -313,12 +444,11 @@ class CombineLidarLane:
         # print(self.tracePath[point15th:])
         
         if isRight:
-            self.straightVel = straightVel
+            self.straightVel = straightVel    # straightVel
             self.turnVel = -turnVel
-            # self.updateVelocity()
             return
         
-        self.straightVel = straightVel
+        self.straightVel = straightVel    # straightVel
         self.turnVel = turnVel
 
 
