4db24063e724a78841d7b93d2b2b26b4053b4f14
DuyThinh1415 <duythinh141592@gmail.com>
2023-06-21 10:47:55 +0700

    update src/lane-detect/src

---
+++ src/lane-detect/src/Wpf_AutoTrain/Wpf_AutoTrain/RunPy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+// Version 2
+
+namespace Wpf_EboxVisionSimulation
+{
+    public class RunPy
+    {
+        public string backendLocation;
+        public string backendName;
+        public string imageName;
+        public string visionData;
+        public Func<string, int> triggerDoneFunction;
+        public Process process;
+
+
+
+
+
+    }
+}
+
+
