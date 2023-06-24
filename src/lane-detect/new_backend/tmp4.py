dacdc8cc478184038222a39a1f1a53f4fd6699df
DuyThinh1415 <duythinh141592@gmail.com>
2023-06-24 21:53:05 +0700

    update document for Thinh

--- src/lane-detect/new_backend/tmp4.py
+++
@@ -1,7 +0,0 @@
-import socket
-import time
-import cv2
-
-tmp = {"tl": [-451, 893], "bl": [539, -843], "tr": [-222, -976], "br": [327, 946]}
-
-print(cv2.clipLine((0,0,128,128),tmp["tl"],tmp["bl"]))
